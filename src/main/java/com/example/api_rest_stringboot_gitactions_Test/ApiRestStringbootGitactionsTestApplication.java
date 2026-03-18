585d61466c35f44cd92ff04216bcb121a66fc690
Doia <leonardogh75@gmail.com>
2026-03-18 11:23:07 -0600

    primer Commit

---
+++ src/main/java/com/example/api_rest_stringboot_gitactions_Test/ApiRestStringbootGitactionsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_rest_stringboot_gitactions_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestStringbootGitactionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestStringbootGitactionsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_rest_stringboot_gitactions_Test/ApiRestStringbootGitactionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_rest_stringboot_gitactions_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestStringbootGitactionsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
